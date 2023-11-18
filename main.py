9ee59489edb54d4949a3752f676ae4f529c65a4f
Fuiro <jacksmith547645@gmail.com>
2023-11-18 17:30:26 +0200

    logika

--- main.py
+++ main.py
@@ -1,47 +1,81 @@
-from random import choice as r
-from random import shuffle as s
-from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QButtonGroup, QRadioButton, QPushButton, QLabel, QSpinBox
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication
+from random import choice, shuffle
+from time import sleep
+a = QApplication([])
+
 from main_window import *
 
-app = QApplication([])
-
-window = QWidget()
-app.exec_()
 
 class Question():
-    def __init__(self, text, question, var, tryy, success):
+    def __init__(self,text, right_answer, ans2,ans3, ans4):
         self.text = text
-        self.question = question
-        self.var1 = var[0]
-        self.var2 = var[1]
-        self.var3 = var[2]
-        self.var4 = var[3]
-        self.tryy = tryy
+        self.right_answer = right_answer
+        self.ans2= ans2
+        self.ans3 = ans3
+        self.ans4 = ans4
+        self.attempts = 0
         self.success = 0
-
     def get_text(self):
         return self.text
     def got_right(self):
-        self.tryy += 1
+        self.attempts += 1
         self.success += 1
-
     def got_wrong(self):
-        self.tryy += 1
+        self.attempts +=1
 
 
-quest1 = Question("Яблуко", ["apple", "aplle", "applu", "pinapple"])
-quest2 = Question("Дім", ["house", "hourse", "hour", "harry"])
-quest3 = Question("Миша", ["mouse", "mase", "mikiyiyiyiyiyi", "mountidju"])
-quest4 = Question("Число", ["number", "mulubulu", "numbur", "hum"])
+question1 = Question('Яблуко', 'apple', 'application', 'pinapple', 'apply')
+question2 = Question('Дім', 'house', 'horse', 'hurry', 'hour')
+question3 = Question('Миша', 'mouse', 'mouth', 'muse', 'museum')
+question4 = Question('Число', 'number', 'digit', 'amount', 'summary')
 
-radio_list = [right_answer, var2, var3, var4]
-quest_list = [var_1, var_2, var_3, var_4]
-current_quest = quest1
 
-def new_question(self):
-        current_quest = r(s(quest_list))
-        lb_question.setText(current_quest.get_text())
-        lb_right_answer.setText()
-        s(radio_list)
-        radio_list[0].setText(current_quest.right_answer)
+radio_list = [rb_ans1, rb_ans2, rb_ans3, rb_ans4]
+question_list = [question1, question2,question3, question4]
+
+def new_question():
+    global current_question
+    current_question = choice(question_list)
+    lb_question.setText(current_question.get_text())
+    lb_right_answer.setText(current_question.right_answer)
+    shuffle(radio_list)
+    radio_list[0].setText(current_question.right_answer)
+    radio_list[1].setText(current_question.ans2)
+    radio_list[2].setText(current_question.ans3)
+    radio_list[3].setText(current_question.ans4)
+
+new_question()
+
+def check_result():
+    RadioGroup.setExclusive(False)
+    for answer in radio_list:
+        if answer.isChecked():
+            if answer.text() == lb_right_answer.text:
+               
+               lb_result.setText('Правильно')
+               current_question.got_right()
+            else:
+                lb_result.setText('Неправильно')
+                current_question.got_wrong()
+                answer.setChecked(False)
+
+def switch_screen():
+    if btn_next.text() == "Відповісти":
+        check_result()
+        gb_question.hide()
+        gb_answer.show()
+        btn_next.setText("Наступне запитання")
+    else:
+        new_question()
+        gb_question.show()
+        gb_answer.hide()
+        btn_next.setText("Відповісти")
+def rest():
+    window.hide()
+    sleep(int(sp_rest.value()))
+    window.show()
+
+btn_rest.clicked.connect(rest)
+btn_next.clicked.connect(switch_screen)
+window.show()
+a.exec_()
